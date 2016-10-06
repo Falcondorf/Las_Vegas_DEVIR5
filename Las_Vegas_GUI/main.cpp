5cf1e53a1be8a51faa260a71008bd7fd775252bc
Falcondorf <39293@heb.be>
2016-10-06 19:15:56 +0200

    modif size fennetre ouverture et position

--- Las_Vegas_GUI/main.cpp
+++ Las_Vegas_GUI/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     titleWindow tw;
     tw.setWindowTitle("Bienvenue");
+    tw.setFixedSize(0,0);
+    tw.move(QPoint(400,100));
     tw.show();
 
     return a.exec();
