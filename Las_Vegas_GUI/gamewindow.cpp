9f1adc282b7bdad9072a7e3333cb34edb0dcc789
Falcondorf <39293@heb.be>
2016-10-06 21:03:07 +0200

    Déclaration des widget et layout définissant la fenêtre de jeu

--- Las_Vegas_GUI/gamewindow.cpp
+++ Las_Vegas_GUI/gamewindow.cpp
@@ -1,5 +1,14 @@
 #include "gamewindow.h"
 
-gameWindow::gameWindow(Game *myGame, QWidget *parent) :QWidget(parent){
+gameWindow::gameWindow(Game *myGame, QWidget *parent) :QWidget(parent), theGame_(myGame){
+    superRoot_ = new QHBoxLayout(this);
+    casinoDisp_ = new QGridLayout(this);
+    rightPannel_ = new QVBoxLayout(this);
+    playerInfos_ = new QGroupBox(this);
+    rollButton_ = new QPushButton("Lancer");
+    dicesDisp_ = new QGridLayout;
+    sbBet_ = new QSpinBox;
+    lBet_ = new QLabel("Valeur a déposer");
+    buttonBet_ = new QPushButton("Parier");
 
 }
--- Las_Vegas_GUI/gamewindow.h
+++ Las_Vegas_GUI/gamewindow.h
@@ -1,10 +1,29 @@
 #ifndef GAMEWINDOW_H
 #define GAMEWINDOW_H
 #include <QWidget>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QGroupBox>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QPixmap>
 #include "game.h"
 
 class gameWindow : public QWidget
 {
+    QHBoxLayout *superRoot_;
+    QGridLayout *casinoDisp_;
+    QVBoxLayout *rightPannel_;
+    QGroupBox *playerInfos_;
+    QPushButton *rollButton_;
+    QGridLayout *dicesDisp_;
+    QHBoxLayout *sbBet_;
+    QLabel *lBet_;
+    QPushButton *buttonBet_;
+
+    Game *theGame_;
 
 public:
     explicit gameWindow(Game *myGame, QWidget *parent = 0);
